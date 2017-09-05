7e6a0a9639d8afb7b592edd2baa70f69d50256a0
Oemed <oal-ali@roc-dev.com>
2017-09-05 13:20:06 +0200

    opdracht 1

---
+++ src/H01/InleidingJava.java
@@ -0,0 +1,12 @@
+package H01;
+
+/**
+ * Created by Oemed on 05-09-17.
+ */
+
+public class InleidingJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo Roc-DEV!");
+    }
+}
